882c3c62abf5cae5c95c738b6a657d3b820ead51
Aaron Alder <aaron.m.alder@gmail.com>
2015-11-24 22:47:03 -0600

    added requests files

---
+++ client/views/requests/requests.js
@@ -0,0 +1,54 @@
+Template.newrequest.onCreated(function(){
+    this.subscribe('requests');
+});
+
+Template.myrequests.onCreated(function(){
+    this.subscribe('requests');
+});
+Template.myRequestBody.onRendered(function(){
+    $(".collapsible").collapsible({
+        accordion: true
+    });
+});
+Template.myrequests.helpers({
+    "requests": function(){
+        var requests = Requests.find({"owner": Meteor.userId()}, {sort: {createdAt: -1}}).fetch();
+        for(r in requests){
+          var owner = Meteor.users.findOne({"_id":requests[r].owner});
+          requests[r].ownerName = owner.profile.firstname + " " + owner.profile.lastname;
+        }
+        return requests;
+    }
+});
+Template.myrequests.events({
+    "click .deleteRequest": function(){
+        Meteor.call('deleteRequest',this._id);
+        Materialize.toast('Removed Request', 3000, 'red');
+    }
+});
+var requestHooks = {
+  before: {
+    insert: function(doc) {
+    	doc.requestor = Meteor.userId();
+    	return doc;
+    }
+  },
+  after: {
+    // Replace `formType` with the form `type` attribute to which this hook applies
+    insert: function(error, result) {
+    	if(!error)
+    	{
+    		FlowRouter.go("/myrequests");
+    		Materialize.toast('Added Request!', 3000, 'green')
+    	}
+    },
+    update: function(error, result) {
+    	if(!error)
+    	{
+    		FlowRouter.go("/myrequests");
+    		Materialize.toast('Updated Request!', 3000, 'green')
+    	}
+    },
+  },
+};
+AutoForm.addHooks(['insertRequest','updateRequest'], requestHooks);
